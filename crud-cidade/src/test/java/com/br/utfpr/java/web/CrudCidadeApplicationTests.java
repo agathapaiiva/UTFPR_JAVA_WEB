f0809f21586b140c411258d961c9f3acc9ad1879
Agatha Paiva <agathahost@gmail.com>
2021-04-18 16:28:49 -0300

    crud-cidades

---
+++ crud-cidade/src/test/java/com/br/utfpr/java/web/CrudCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.utfpr.java.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
