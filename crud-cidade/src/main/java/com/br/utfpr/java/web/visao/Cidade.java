d9ca45519a832bb1f61a567780c1530e0ff4e2ac
Agatha Paiva <agathahost@gmail.com>
2021-04-21 23:03:55 -0300

    página dinamica utilizando o Freemarker - Spring Boot

---
+++ crud-cidade/src/main/java/com/br/utfpr/java/web/visao/Cidade.java
@@ -0,0 +1,20 @@
+package com.br.utfpr.java.web.visao;
+
+public final class Cidade {
+	private final String nome;
+	private final String estado;
+	
+	public Cidade(final String nome, final String estado) {
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+	
+}
